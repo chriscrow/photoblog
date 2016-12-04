53d7d9b3772e29c4bc0073ed058abce57de9d303
Chris Chen <zchen2000@qq.com>
2016-12-04 10:51:13 +0800

    add relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126171900) do
+ActiveRecord::Schema.define(version: 20161203170344) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "content"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20161126171900) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
