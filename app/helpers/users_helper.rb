4f12ba8c0238bea67a8f3bbae96012bcc233b25d
Chris Chen <zchen2000@qq.com>
2016-12-17 00:43:36 +0800

    change avatar css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,6 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.username.downcase)
     size = options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-    image_tag(gravatar_url, alt:user.nickname, class:"gravatar")
+    content_tag(:div, image_tag(gravatar_url, alt:user.nickname), class: "gravatar")
   end
 end
