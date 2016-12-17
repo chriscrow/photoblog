d5d95b9f3c29dec374df8addd48522228865ea5b
Chris Chen <zchen2000@qq.com>
2016-12-17 11:17:40 +0800

    change index page theme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if signed_in?
       @feed_items = current_user.feed.paginate(per_page:10, page: params[:page])
+    else
+      @feed_items = Article.limit(10).paginate(per_page:10, page: params[:page], total_entries:30)
     end
   end
 end
